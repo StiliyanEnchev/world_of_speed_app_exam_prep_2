d2d134b41cdc0b1c9ddca61166ba3df0700d9857
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2024-10-25 15:41:01 +0300

    Car edit fixed

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 
-from profiles.views import IndexPageView, ProfileCreateView
+from profiles.views import IndexPageView, ProfileCreateView, ProfileDetailsView
 
 urlpatterns = [
     path('', IndexPageView.as_view(), name='home'),
     path('profile/', include([
-            path('create/', ProfileCreateView.as_view(), name='create-profile')]
+            path('create/', ProfileCreateView.as_view(), name='create-profile'),
+            path('details/', ProfileDetailsView.as_view(), name='profile-details')]
         ))
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView, CreateView, DetailView
 
+from common.utils import get_profile
 from profiles.forms import ProfileCreateForm
 from profiles.models import Profile
 
@@ -20,6 +21,14 @@ class ProfileCreateView(CreateView):
 class ProfileDetailsView(DetailView):
     model = Profile
     template_name = 'profile-details.html'
+    def get_object(self, queryset=None):
+        return get_profile()
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        total_sum = sum(car.price for car in get_profile().cars.all())
 
+        context['total_sum'] = total_sum
+        context['items'] = Profile.objects.all()  # Fetch all items from YourModel
 
+        return context
